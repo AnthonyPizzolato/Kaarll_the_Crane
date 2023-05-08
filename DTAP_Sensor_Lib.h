5003691cb8476d040da45f22f24147c02687c1dc
Devon Thomas <104389195+DevT1@users.noreply.github.com>
2023-05-07 23:57:51 -0400

    Create DTAP_Sensor_Lib.h

---
+++ DTAP_Sensor_Lib.h
@@ -0,0 +1,14 @@
+#ifndef __DTAP_SENSOR_LIB_H__
+#define __DTAP_SENSOR_LIB_H__
+
+//NEED a Function to read in values from a POT
+int potRead(void);
+//NEED a function that converts Values read from pot into angles/PWM signals for servo
+void PWM_Logic(int);
+//NEED a function that initialize all of the pins that will be used by the pot and servo
+void ADC_Intializer(void);
+void PWM_Intializer(void);
+//Only use this function if Anthony wantes to use it
+void PWM_PIT_ISR(void);
+
+#endif
